6da7657c9b8159f638a3962c0ff7b19b28194200
joeydeeds <joey.deeds@gmail.com>
2018-07-27 12:33:34 -0500

    finished and ready for submission

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -4,64 +4,102 @@ import Web3 from 'web3'
 import { CONTRACT_NAME, CONTRACT_ADDRESS } from '../config'
 // import { etherscan_api_key } from '../secret'
 import KittyCoreABI from '../contracts/KittyCoreABI.json'
-
 const web3 = new Web3(new Web3.providers.HttpProvider())
-
-// 1512627536 = December 07 2017
 
 class Browser extends Component {
   constructor(props, context) {
     super(props)
     this.state = {
       kittyID: '',
-      data: []
+      data: [],
+      imgUrl: '',
+      notFound: false
     }
   }
+
+  // State handling functions
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value })
   }
 
-  handleSubmit = async event => {
-    event.preventDefault()
+  bigKittyError = () => {
+    this.setState({
+      kittyID: '',
+      data: [],
+      imgUrl: '',
+      notFound: true
+    })
+  }
+
+  // Kitty Helper Functions
+
+  getImage = () => {
+    fetch(`https://api.cryptokitties.co/kitties/${this.state.kittyID}`)
+      .then(res => res.json())
+      .catch(err => console.log(err))
+      .then(result => result ? this.setState({ imgUrl: result.image_url }) : this.bigKittyError())
+  }
+
+  convertBirthday = time => {
+    const newTime = new Date(0)
+    newTime.setUTCSeconds(time)
+    const options = { year: 'numeric', month: 'long', day: 'numeric' }
+    return newTime.toLocaleDateString('en-US', options)
+  }
+
+  randomKitty = async () => {
+    let num = Math.random() * (862515 - 1) + 1;
+    this.setState({kittyID: Math.floor(num)})
+    this.fetchKitty()
+  }
+
+  fetchKitty = async () => {
     const { getKitty } = this.context.drizzle.contracts.CryptoKitties.methods
     const kitty = await getKitty(this.state.kittyID).call()
-    this.setState({ data: kitty })
+    this.setState({ data: kitty, notFound: false })
+    this.getImage()
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.fetchKitty()
   }
 
   componentDidMount() {
     const { drizzle } = this.context
-    // If Drizzle is initialized (and therefore web3, accounts and contracts), continue.
     const web3 = new Web3(window.web3.currentProvider)
-    // Initialize the contract instance
     const kittyContract = new web3.eth.Contract(
-      KittyCoreABI, // import the contracts's ABI and use it here
+      KittyCoreABI,
       CONTRACT_ADDRESS
     )
-    // Add the contract to the drizzle store
-    this.context.drizzle.addContract({
+    drizzle.addContract({
       contractName: CONTRACT_NAME,
       web3Contract: kittyContract
     })
-
   }
 
   render() {
     const kitty = this.state.data
-    const time = new Date(0)
     return (
       <div className="browser">
         <h1>Kitty Browser</h1>
         <form onSubmit={this.handleSubmit}>
           <label>Kitty ID:</label>
+          <br />
           <input
             type="number"
             name="kittyID"
-            value={this.state.kittyName}
+            value={this.state.kittyID}
             onChange={this.handleChange}
+            max="862515"
           />
           <input type="submit" value="Find Kitty" />
+          <button type="button" onClick={() => this.randomKitty()}>Random Kitty</button>
         </form>
+
+        {this.state.notFound ?  <h3>Kitty was not found!</h3> : null}
+
         {this.state.data.genes ? (
           <div>
             <h3>Genes</h3>
@@ -69,7 +107,8 @@ class Browser extends Component {
             <h3>Generation</h3>
             <p>{kitty.generation}</p>
             <h3>Birth Time</h3>
-            <p>{time.setUTCSeconds(kitty.birthTime)}</p>
+            <p>{this.convertBirthday(kitty.birthTime)}</p>
+            <img src={this.state.imgUrl} alt="kitty" />
           </div>
         ) : null}
       </div>
